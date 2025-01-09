d52451a4736b34263aa5f6f63169e493099f3f45
AntonioSarti <59610410+AntonioSarti@users.noreply.github.com>
2025-01-09 17:28:47 -0300

    Update collisions.hpp

--- fcg/include/collisions.hpp
+++ fcg/include/collisions.hpp
@@ -34,16 +34,13 @@ extern BoundingBox playerBox;
 extern BoundingBox enemyBox;
 extern std::vector<BoundingBox> treeBoxes;
 
-// Função que calcula o tempo que irá levar para um plano e um AABB colidirem, assumindo que
-// o plano é estático, o AABB se move a uma velocidade constante para baixo e o AABB
-// inicialmente não intersecciona o plano. Um tempo negativo indica que os dois objetos nunca
-// irão colidir.
+// Função que calcula o tempo que irá levar para um plano e um AABB colidirem.
 float CalculateAABBToPlaneCollisionTime(float plane_height, float bbox_min_height, float vel);
 
 // Função que calcula se um raio intersecta um AABB
 bool RayIntersectsAABB(glm::vec4 &ray_origin, glm::vec4 &ray_direction, glm::vec4 &aabb_min, glm::vec4 &aabb_max);
 
-// Checa uma colisão entre 2 bounding boxes
+// Verifica uma colisão entre 2 bounding boxes
 bool CheckCollision(const BoundingBox &box1, const BoundingBox &box2);
 
 // Verifica se o jogador está entre os limites do mapa
